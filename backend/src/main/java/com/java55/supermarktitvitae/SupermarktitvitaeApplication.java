93125412ee2284300a87aca5bba487a7d4164202
Michelvdm83 <michelvdm@gmail.com>
2024-06-03 11:54:52 +0200

    backend created

---
+++ backend/src/main/java/com/java55/supermarktitvitae/SupermarktitvitaeApplication.java
@@ -0,0 +1,13 @@
+package com.java55.supermarktitvitae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarktitvitaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarktitvitaeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/java55/supermarktitvitae/SupermarktitvitaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java55.supermarktitvitae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarktitvitaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
