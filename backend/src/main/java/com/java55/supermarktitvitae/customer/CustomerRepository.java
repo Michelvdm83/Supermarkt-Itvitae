4c80542e904f745b9d95c0b15900f28025594445
Jules95Game <romp.julian1@gmail.com>
2024-06-04 15:20:02 +0200

    Added code to CustomerRepository

--- backend/src/main/java/com/java55/supermarktitvitae/customer/CustomerRepository.java
+++ backend/src/main/java/com/java55/supermarktitvitae/customer/CustomerRepository.java
@@ -1,4 +1,6 @@
 package com.java55.supermarktitvitae.customer;
 
-public interface CustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, String> {
 }
