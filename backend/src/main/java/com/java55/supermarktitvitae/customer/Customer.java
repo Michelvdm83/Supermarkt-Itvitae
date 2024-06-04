9bd9e6a51f094116899f5dc3c047a4895f490577
Jules95Game <romp.julian1@gmail.com>
2024-06-04 14:42:19 +0200

    Added code to Customer class

--- backend/src/main/java/com/java55/supermarktitvitae/customer/Customer.java
+++ backend/src/main/java/com/java55/supermarktitvitae/customer/Customer.java
@@ -1,4 +1,21 @@
 package com.java55.supermarktitvitae.customer;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Customer {
+
+    private String name;
+
+    @Id
+    private String email;
+
+    private String password;
 }
