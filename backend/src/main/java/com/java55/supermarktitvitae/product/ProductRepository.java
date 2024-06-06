2923f124d85af2983e207beb4d3defa5bdec0f76
JavaGerben <g.j.mertens@outlook.com>
2024-06-06 15:04:53 +0200

    error while running

--- backend/src/main/java/com/java55/supermarktitvitae/product/ProductRepository.java
+++ backend/src/main/java/com/java55/supermarktitvitae/product/ProductRepository.java
@@ -2,5 +2,8 @@ package com.java55.supermarktitvitae.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, String> {
+    Optional<Product> findByName(String productName);
 }
--- backend/src/main/java/com/java55/supermarktitvitae/shoppingcart/ShoppingCartController.java
+++ backend/src/main/java/com/java55/supermarktitvitae/shoppingcart/ShoppingCartController.java
@@ -1,15 +1,42 @@
 package com.java55.supermarktitvitae.shoppingcart;
 
+import com.java55.supermarktitvitae.customer.CustomerRepository;
+import com.java55.supermarktitvitae.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 @RequestMapping("api/v1/shoppingcarts")
 @RequiredArgsConstructor
 public class ShoppingCartController {
-    private ShoppingCartRepository shoppingCartRepository;
+    private final ShoppingCartRepository shoppingCartRepository;
+    private final CustomerRepository customerRepository;
+    private final ProductRepository productRepository;
+
+    @PostMapping("{customerEmail}/{productName}")
+    public ResponseEntity<ShoppingCart> AddProduct(@PathVariable String customerEmail, @PathVariable String productName, UriComponentsBuilder ucb) {
+//        if (customerEmail == null) return ResponseEntity.notFound().build();
+//        if (productName == null) return ResponseEntity.notFound().build();
+//
+//        var possibleCustomer = customerRepository.findById(customerEmail);
+//        if (possibleCustomer.isEmpty()) return ResponseEntity.notFound().build();
+//        Customer customer = possibleCustomer.get();
+//
+//        ShoppingCart shoppingCart = new ShoppingCart(customer);
+//        shoppingCartRepository.save(shoppingCart);
+//
+//        var possibleProduct = productRepository.findByName(productName);
+//        if (possibleProduct.isEmpty()) return ResponseEntity.notFound().build();
+//        Product product = possibleProduct.get();
+//
+//        var uri = ucb.path("api/v1/shoppingcarts/{id}").buildAndExpand(shoppingCart.getId()).toUri();
+//        return ResponseEntity.created(uri).body(shoppingCartRepository.save(shoppingCart));
+        return ResponseEntity.ok().build();
+    }
 
 }
