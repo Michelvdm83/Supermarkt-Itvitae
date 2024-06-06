f3305965710583a06ba58006ae76afbab8e5083d
JavaGerben <g.j.mertens@outlook.com>
2024-06-06 10:26:14 +0200

    added @AllArgsConstructor instead of writing it myself.

--- backend/src/main/java/com/java55/supermarktitvitae/product/Product.java
+++ backend/src/main/java/com/java55/supermarktitvitae/product/Product.java
@@ -2,12 +2,14 @@ package com.java55.supermarktitvitae.product;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class Product {
@@ -17,12 +19,4 @@ public class Product {
     private String description;
     private Category category;
     private Double salesPrice;
-
-    public Product(String name, double price, String description, Category category, Double salesPrice) {
-        this.name = name;
-        this.price = price;
-        this.description = description;
-        this.category = category;
-        this.salesPrice = salesPrice;
-    }
 }
