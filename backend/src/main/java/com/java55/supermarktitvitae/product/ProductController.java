9109aa7a8f6eaf43a27144c8adbe441b963448d5
Jules95Game <romp.julian1@gmail.com>
2024-06-06 11:46:44 +0200

    Added getProductByName endpoint

--- backend/src/main/java/com/java55/supermarktitvitae/product/ProductController.java
+++ backend/src/main/java/com/java55/supermarktitvitae/product/ProductController.java
@@ -1,12 +1,24 @@
 package com.java55.supermarktitvitae.product;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/products")
 @RequiredArgsConstructor
 public class ProductController {
     private final ProductRepository productRepository;
+
+    @GetMapping("{name}")
+    public ResponseEntity<Product> getProductByName(@RequestParam String name) {
+        Optional<Product> product = productRepository.findByName(name);
+        if (product.isEmpty()) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(product.get());
+    }
 }
