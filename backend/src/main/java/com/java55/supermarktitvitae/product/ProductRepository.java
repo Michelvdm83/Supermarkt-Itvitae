79d21af2858039444e4a9837603af5c0dfd5966b
EGerritsen99 <145434680+EGerritsen99@users.noreply.github.com>
2024-06-07 10:20:32 +0200

    searchbar fix

--- backend/src/main/java/com/java55/supermarktitvitae/product/ProductRepository.java
+++ backend/src/main/java/com/java55/supermarktitvitae/product/ProductRepository.java
@@ -2,5 +2,10 @@ package com.java55.supermarktitvitae.product;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, String> {
+
+    public List<Product> findByNameContainsIgnoreCase(String contains);
+
 }
--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,7 +1,19 @@
 import { useState } from 'react';
+import axios from 'axios';
+
 
 export default function SearchBar() {
+
     const [text, setText] = useState("");
+
+    function search() {
+        console.log('search called');
+        axios.get('http://localhost:8080/api/v1/products/searchbar?contains=' + text)
+        .then(response => {
+            console.log(response);
+        })
+    }
+
     return (
         <div className="searchbar">
             <input 
@@ -10,7 +22,7 @@ export default function SearchBar() {
             value={text}
             onChange={(event) => setText(event.target.value)}
             />
-            <button onClick={() => console.log(text)}>Zoek</button>
+            <button onClick={search}>Zoek</button>
         </div>
     )
 }
