9109172236e621dc43de7a36fdc51074c5cd7ee1
JavaGerben <g.j.mertens@outlook.com>
2024-06-07 12:18:15 +0200

    creating shopping cart + add first product works

--- backend/src/main/java/com/java55/supermarktitvitae/customer/CustomerController.java
+++ backend/src/main/java/com/java55/supermarktitvitae/customer/CustomerController.java
@@ -8,6 +8,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/customers")
 @RequiredArgsConstructor
 public class CustomerController {
-
     private final CustomerRepository customerRepository;
 }
--- backend/src/main/java/com/java55/supermarktitvitae/shoppingcart/ShoppingCartController.java
+++ backend/src/main/java/com/java55/supermarktitvitae/shoppingcart/ShoppingCartController.java
@@ -4,13 +4,14 @@ import com.java55.supermarktitvitae.customer.Customer;
 import com.java55.supermarktitvitae.customer.CustomerRepository;
 import com.java55.supermarktitvitae.product.Product;
 import com.java55.supermarktitvitae.product.ProductRepository;
+import com.java55.supermarktitvitae.shoppingcartproduct.ShoppingCartProduct;
+import com.java55.supermarktitvitae.shoppingcartproduct.ShoppingCartProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/shoppingcarts")
@@ -19,9 +20,24 @@ public class ShoppingCartController {
     private final ShoppingCartRepository shoppingCartRepository;
     private final CustomerRepository customerRepository;
     private final ProductRepository productRepository;
+    private final ShoppingCartProductRepository shoppingCartProductRepository;
 
-    @PostMapping("{customerEmail}/{productName}")
-    public ResponseEntity<ShoppingCart> AddProduct(@PathVariable String customerEmail, @PathVariable String productName, UriComponentsBuilder ucb) {
+    @GetMapping("{id}")
+    public ResponseEntity<ShoppingCart> GetById(@PathVariable UUID id) {
+        if (id == null) return ResponseEntity.badRequest().build();
+
+        var possibleShoppingCart = shoppingCartRepository.findById(id);
+        return possibleShoppingCart.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("{customerEmail}/{productName}/{quantity}")
+    public ResponseEntity<ShoppingCart> AddProduct(
+            @PathVariable String customerEmail,
+            @PathVariable String productName,
+            @PathVariable int quantity,
+            
+            UriComponentsBuilder ucb
+    ) {
         if (customerEmail == null) return ResponseEntity.notFound().build();
         if (productName == null) return ResponseEntity.notFound().build();
 
@@ -30,14 +46,17 @@ public class ShoppingCartController {
         Customer customer = possibleCustomer.get();
 
         ShoppingCart shoppingCart = new ShoppingCart(customer);
-        shoppingCartRepository.save(shoppingCart);
+        ShoppingCart savedShoppingCart = shoppingCartRepository.save(shoppingCart);
 
         var possibleProduct = productRepository.findByName(productName);
         if (possibleProduct.isEmpty()) return ResponseEntity.notFound().build();
         Product product = possibleProduct.get();
 
+        ShoppingCartProduct shoppingCartProduct = new ShoppingCartProduct(shoppingCart, product, quantity);
+        shoppingCartProductRepository.save(shoppingCartProduct);
+
         var uri = ucb.path("api/v1/shoppingcarts/{id}").buildAndExpand(shoppingCart.getId()).toUri();
-        return ResponseEntity.created(uri).body(shoppingCartRepository.save(shoppingCart));
+        return ResponseEntity.created(uri).body(savedShoppingCart);
     }
 
 }
