b60dfc2dfe24aa8943b35495449955b3aa57d0b2
Jules95Game <romp.julian1@gmail.com>
2024-06-10 15:13:39 +0200

    Added useParams to get productName from url

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 export default function ProductPage() {
   const [product, setProduct] = useState([]);
+  const { productName } = useParams();
 
   useEffect(getProduct, []);
 
   function getProduct() {
-    fetch("http://localhost:8080/api/v1/products/Kaas")
+    fetch(`http://localhost:8080/api/v1/products/${productName}`)
       .then((response) => response.json())
       .then((body) => setProduct(body))
       .catch((error) => console.log(error));
