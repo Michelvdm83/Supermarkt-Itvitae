bd42ed60fe0120b29287e05c847dbb0b1a156656
JavaGerben <g.j.mertens@outlook.com>
2024-06-14 15:09:18 +0200

    homebutton component logo name changed

--- frontend/src/components/HomeButton/HomeButton.jsx
+++ frontend/src/components/HomeButton/HomeButton.jsx
@@ -5,10 +5,7 @@ export default function HomeButton({}) {
 
   return (
     <button onClick={() => navigate("")}>
-      <img
-        className="w-20 h-20 ml-4 mt-2"
-        src="src\images\logo_met_tekst.png"
-      ></img>
+      <img className="w-20 h-20 ml-4 mt-2" src="src\images\logo.png"></img>
     </button>
   );
 }
