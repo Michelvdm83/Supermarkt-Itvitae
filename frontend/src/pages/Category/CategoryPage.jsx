c1558f856d70a5742ac4eb506ad137cf3a6c8db4
Jules95Game <romp.julian1@gmail.com>
2024-06-17 13:11:57 +0200

    Added return statement to CategoryPage

--- frontend/src/pages/Category/CategoryPage.jsx
+++ frontend/src/pages/Category/CategoryPage.jsx
@@ -2,5 +2,23 @@ import { useEffect, useState } from "react";
 import CategoryBox from "./CategoryBox";
 
 export default function CategoryPage() {
-  return <>CategoryPage works!</>;
+  const [categories, setCategories] = useState();
+
+  useEffect(getCategories, []);
+
+  function getCategories() {
+    fetch(`http://localhost:8080/api/v1/categories`)
+      .then((response) => response.json())
+      .then((body) => setCategories(body))
+      .catch((error) => console.log(error));
+  }
+
+  return (
+    <>
+      <div className="flex flex-row mt-16 flex-wrap">
+        {categories &&
+          categories.map((category) => <CategoryBox name={category} />)}
+      </div>
+    </>
+  );
 }
