58cad68436d108ac873cd1153eb52cdb0751a7f4
JavaGerben <g.j.mertens@outlook.com>
2024-06-17 11:15:31 +0200

    used addproduct button css

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -2,11 +2,14 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import AddProductButton from "../components/AddProductButton/AddProductButton";
 
 export default function ProductPage() {
   const [product, setProduct] = useState({});
-  const { productName } = useParams();
   const [quantity, setQuantity] = useState(1);
+  const { productName } = useParams();
+
+  const role = sessionStorage.getItem("ROLE");
 
   useEffect(getProduct, []);
 
@@ -26,7 +29,6 @@ export default function ProductPage() {
     if (quantity <= 0) {
       alert("Het produkt aantal mag niet 0 of lager zijn.");
     } else {
-      console.log("succes");
       axios
         .post(
           "http://localhost:8080/api/v1/shoppingcarts",
@@ -59,23 +61,27 @@ export default function ProductPage() {
     console.log(quantity);
   };
 
+  let buttonCSS =
+    "text-white rounded-2xl font-extrabold h-10  mr-2 p-2 flex justify-center items-center ";
+  buttonCSS += product.salesPrice ? "bg-nn-pink" : "bg-nn-green";
+
   return (
     <>
       <p>{product.name}</p>
       <p>{NLEuro.format(productPrice(product))}</p>
       <p>{product.category}</p>
       <p>{product.description}</p>
-      <p>
-        <input
-          value={quantity}
-          onChange={(e) => setQuantity(e.target.value)}
-        ></input>
-      </p>
-      <p>
-        <button onClick={addProductToCart} className="bottom-auto">
-          Add to cart
-        </button>
-      </p>
+      {role === "customer" && (
+        <>
+          <input
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          ></input>
+          <button className={buttonCSS} onClick={addProductToCart}>
+            Voeg toe
+          </button>
+        </>
+      )}
     </>
   );
 }
