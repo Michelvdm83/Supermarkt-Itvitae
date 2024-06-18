006f6a163c8a085ab68efd2215cc26ba1d2b3865
Jules95Game <romp.julian1@gmail.com>
2024-06-18 15:30:25 +0200

    AddProduct gives a warning when a non manager tries to open the page

--- frontend/src/pages/addProduct/AddProduct.jsx
+++ frontend/src/pages/addProduct/AddProduct.jsx
@@ -1,7 +1,20 @@
 export default function AddProduct() {
+  const USERNAME_STORAGE_LOCATION = "USERNAME";
+  const TOKEN_STORAGE_LOCATION = "JWT";
+  const ROLE_STORAGE_LOCATION = "ROLE";
+
   return (
     <>
-      <p>Add Product</p>
+      {sessionStorage.getItem(ROLE_STORAGE_LOCATION) !== "manager" && (
+        <div className="my-16 text-2xl">
+          <p>U bent niet gemachtigd deze pagina te bekijken.</p>
+        </div>
+      )}
+      {sessionStorage.getItem(ROLE_STORAGE_LOCATION) === "manager" && (
+        <div>
+          <p>Hallo Manager</p>
+        </div>
+      )}
     </>
   );
 }
