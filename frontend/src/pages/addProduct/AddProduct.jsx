7e342c9dfa966d8667d5bf4f3522bb86f689566d
Jules95Game <romp.julian1@gmail.com>
2024-06-18 16:04:46 +0200

    Added two input fields

--- frontend/src/pages/addProduct/AddProduct.jsx
+++ frontend/src/pages/addProduct/AddProduct.jsx
@@ -7,12 +7,41 @@ export default function AddProduct() {
     <>
       {sessionStorage.getItem(ROLE_STORAGE_LOCATION) !== "manager" && (
         <div className="my-16 text-2xl">
-          <p>U bent niet gemachtigd deze pagina te bekijken.</p>
+          <p>U bent niet gemachtigd om deze pagina te bekijken.</p>
         </div>
       )}
       {sessionStorage.getItem(ROLE_STORAGE_LOCATION) === "manager" && (
-        <div>
-          <p>Hallo Manager</p>
+        <div className="flex flex-col">
+          <p>Een product toevoegen:</p>
+          <div className="my-2">
+            <input
+              className="border-2 focus:outline-none invalid:border-red-700 valid:border-green-700"
+              type="text"
+              placeholder="product naam"
+              onChange={(event) => event}
+              required
+              autoComplete="false"
+              key="mail"
+            />
+            <button className="mx-2" onClick={() => console.log("click")}>
+              Opslaan
+            </button>
+          </div>
+
+          <div className="my-2">
+            <input
+              className="border-2 focus:outline-none invalid:border-red-700 valid:border-green-700"
+              type="text"
+              placeholder="product beschrijving"
+              onChange={(event) => event}
+              required
+              autoComplete="false"
+              key="description"
+            />
+            <button className="mx-2" onClick={() => console.log("click")}>
+              Opslaan
+            </button>
+          </div>
         </div>
       )}
     </>
