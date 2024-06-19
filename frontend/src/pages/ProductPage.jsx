3562230fdb017a4b4af642592bb111ee349b2637
EGerritsen99 <145434680+EGerritsen99@users.noreply.github.com>
2024-06-19 09:45:30 +0200

    imports fix

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import AddProductButton from "../components/AddProductButton/AddProductButton";
 
 export default function ProductPage() {
   const [product, setProduct] = useState({});
