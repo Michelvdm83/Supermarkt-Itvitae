74594b12c45c81fe01676589569681a0c49a302d
Jules95Game <romp.julian1@gmail.com>
2024-06-20 13:07:27 +0200

    Added input logic

--- frontend/src/pages/addProduct/AddProduct.jsx
+++ frontend/src/pages/addProduct/AddProduct.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
 export default function AddProduct() {
   const USERNAME_STORAGE_LOCATION = "USERNAME";
   const TOKEN_STORAGE_LOCATION = "JWT";
   const ROLE_STORAGE_LOCATION = "ROLE";
+
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState(0);
+  const [salesPrice, setSalesPrice] = useState(null);
+  const [category, setCategory] = useState("");
+
+  function handleSubmit() {
+    console.log("Click");
+  }
 
   return (
     <>
@@ -11,37 +23,53 @@ export default function AddProduct() {
         </div>
       )}
       {sessionStorage.getItem(ROLE_STORAGE_LOCATION) === "manager" && (
-        <div className="flex flex-col">
+        <div className="flex flex-col items-center my-8">
           <p>Een product toevoegen:</p>
-          <div className="my-2">
-            <input
-              className="border-2 focus:outline-none invalid:border-red-700 valid:border-green-700"
-              type="text"
-              placeholder="product naam"
-              onChange={(event) => event}
-              required
-              autoComplete="false"
-              key="mail"
-            />
-            <button className="mx-2" onClick={() => console.log("click")}>
-              Opslaan
-            </button>
+
+          <div className="flex flex-col items-end">
+            <div className="flex my-2">
+              <p className="mx-2">Naam</p>
+              <input
+                className="border-2 focus:outline-none invalid:border-red-700 valid:border-green-700"
+                type="text"
+                placeholder="product naam"
+                value={name}
+                onChange={(event) => {
+                  setName(event.target.value);
+                  console.log(name);
+                }}
+                required
+                minLength={3}
+                autoComplete="false"
+                key="name"
+              />
+            </div>
+
+            <div className="flex my-2">
+              <p className="mx-2">Omschrijving</p>
+              <input
+                className="border-2 focus:outline-none invalid:border-red-700 valid:border-green-700"
+                type="text"
+                placeholder="product omschrijving"
+                value={description}
+                onChange={(event) => {
+                  setDescription(event.target.value);
+                  console.log(description);
+                }}
+                required
+                minLength={5}
+                autoComplete="false"
+                key="description"
+              />
+            </div>
           </div>
 
-          <div className="my-2">
-            <input
-              className="border-2 focus:outline-none invalid:border-red-700 valid:border-green-700"
-              type="text"
-              placeholder="product beschrijving"
-              onChange={(event) => event}
-              required
-              autoComplete="false"
-              key="description"
-            />
-            <button className="mx-2" onClick={() => console.log("click")}>
-              Opslaan
-            </button>
-          </div>
+          <button
+            className="bg-gray-400 rounded my-2 p-1"
+            onClick={() => handleSubmit()}
+          >
+            Opslaan
+          </button>
         </div>
       )}
     </>
