96db6c83e84d92330330a21c50a9b3970a2aa546
Michelvdm83 <michelvdm@gmail.com>
2024-06-20 15:47:09 +0200

    done

--- frontend/src/pages/Sales/Sales.jsx
+++ frontend/src/pages/Sales/Sales.jsx
@@ -21,13 +21,24 @@ export default function Sales() {
     fetchSales();
   }, []);
 
+  let currentCategory = "";
   return (
     <div className="w-1/3 flex flex-col justify-center gap-8">
       <Banner bannerText="In de aanbieding" />
       <ul className=" flex flex-col gap-4">
-        {sales.map((product) => (
-          <ProductBox key={product.name} product={product} />
-        ))}
+        {sales.map((product) => {
+          if (product.category === currentCategory) {
+            return <ProductBox key={product.name} product={product} />;
+          } else {
+            currentCategory = product.category;
+            return (
+              <>
+                <Banner bannerText={product.category} />
+                <ProductBox key={product.name} product={product} />
+              </>
+            );
+          }
+        })}
       </ul>
     </div>
   );
