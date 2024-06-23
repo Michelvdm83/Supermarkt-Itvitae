6097a4de6ebbda2332f5655ba085c6332be4237c
EGerritsen99 <145434680+EGerritsen99@users.noreply.github.com>
2024-06-23 14:13:08 +0200

    Update Categorybanner.jsx

--- frontend/src/components/Banner/Categorybanner.jsx
+++ frontend/src/components/Banner/Categorybanner.jsx
@@ -1,7 +1,7 @@
 export default function CategoryBanner({ bannerText }) {
   return (
     <div className="border-nn-green text-nn-green mb-2 text-l font-extrabold mt-12 border-4 rounded-2xl shadow-xl flex h-10 justify-center items-center">
-      <h1 className="mr-2">Categorie:</h1>
+      <h1 className="mr-2">Categorie -</h1>
       <h1 className="lowercase">{bannerText}</h1>
     </div>
   );
